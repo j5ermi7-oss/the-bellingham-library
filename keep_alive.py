7d6f62e23270f49364e9665a732e81f279fb31a8
j5ermi7-oss <j5ermi7@gmail.com>
2026-07-19 17:24:07 +0300

    Add files via upload

--- keep_alive.py
+++ keep_alive.py
@@ -1,20 +1,17 @@
 from flask import Flask
 from threading import Thread
 import logging
-
 # Disable flask logging to keep console clean
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
-
 app = Flask(__name__)
-
+import os
 @app.route('/')
 def home():
     return "Bot is alive!"
-
 def run():
-    app.run(host='0.0.0.0', port=8080)
-
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
 def keep_alive():
     t = Thread(target=run)
     t.daemon = True
